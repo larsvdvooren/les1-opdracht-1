745fa419c6a4f9a2db3795385b69e0617fab86c2
larsvdvooren <74357425+larsvdvooren@users.noreply.github.com>
2024-09-02 12:06:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ document.title = 'My Magical Home Page';
 const louisa = {
   naam: 'Louisa',
   age: 25,
+  pets: ['Toekan', 'Komodovaraan', 'Capybara'],
   getFullYear: function() {
     const date = new Date();
     const currentYear = date.getFullYear();
@@ -12,9 +13,17 @@ const louisa = {
     console.log('Hallo ik ben ' + this.naam);
     //console.log('Hallo ik ben ' + this.naam + ' ik ben geboren in ' + getFullYear);
   },
+
+  listPets: function() {
+    console.log('Ik heb de volgende huisdieren: ');
+    for(let i = 0; i < this.pets.length; i++) {
+      console.log('Een: ' + this.pets[i])
+    }
+  }
 };
 
 
 
 louisa.talk();
 louisa.getFullYear();
+louisa.listPets();
